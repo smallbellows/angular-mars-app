9f33010d964ca71bcb5911214d049a44ed0fc408
Sara <given.sara@gmail.com>
2016-06-23 15:29:39 -0700

    improved some layout and also updated the readme

--- README.md
+++ README.md
@@ -14,6 +14,4 @@ A mobile-first web app for colonists on Mars to report alien sightings.
 
 I enjoyed working with Angular2, especially the ways it allows an app to interact with a JSON API. I pulled in data from 2 APIs for this project, one for all the data and a WP API for the Colony Blog.
 
-I made a small attempt at trying out Fluid Typography. It's an intriguing concept that I'd like to try out in a more text-heavy situation. 
-
-I would like to figure out a way to provide feedback to the users about not having form fields completed. 
+This project also is my first attempt at trying out Fluid Typography. It's an intriguing concept that I'd like to try out in a more text-heavy situation. 
--- src/app/shared/services/blogpost.service.ts
+++ src/app/shared/services/blogpost.service.ts
@@ -6,22 +6,21 @@ import { IBlogPost } from '../models';
 
 @Injectable()
 export class BlogPostService {
-    
-    blogUrl = 'http://fourth.academy.red/wp-json/wp/v2/posts';
-    
+
+    blogUrl = 'http://fourth.academy.red/wp-json/wp/v2/posts/';
+
     constructor(private http: Http ) {}
-    
+
     getBlogPosts(): Promise<IBlogPost[]> {
-        
+
         return this.http.get(this.blogUrl)
                         .toPromise()
                         .then( response => response.json() )
                         .catch( this.handleError );
     }
-    
+
     private handleError(error: any) {
         console.log('There was an error', error);
         return Promise.reject(error.message || error);
     }
 }
-
